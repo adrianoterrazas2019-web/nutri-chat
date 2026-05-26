70a05f8c58311838a0d538915668fbc87bc2419e
adriano terrazas <adrianoterrazas2019@gmail.com>
2026-05-26 14:33:13 +0200

    run new schema with create meals table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_26_081950) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_26_095240) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "meals", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "description"
+    t.text "nutritional_info"
+    t.text "system_prompt"
+    t.string "title"
+    t.datetime "updated_at", null: false
+  end
 
   create_table "solid_cable_messages", force: :cascade do |t|
     t.binary "channel", null: false
