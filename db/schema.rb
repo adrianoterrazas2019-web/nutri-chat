b94a5e72e8423c84ded639452d4137b3459eb83b
Daniel dos Santos <ddsantos@gmx.de>
2026-05-27 14:22:11 +0200

    setup ruby_llm gem

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,10 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_27_093420) do
 
   create_table "chats", force: :cascade do |t|
     t.datetime "created_at", null: false
+    t.bigint "meal_id", null: false
+    t.string "title"
     t.datetime "updated_at", null: false
+    t.index ["meal_id"], name: "index_chats_on_meal_id"
   end
 
   create_table "meals", force: :cascade do |t|
@@ -32,6 +35,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_27_093420) do
     t.bigint "chat_id", null: false
     t.text "content"
     t.datetime "created_at", null: false
+    t.string "role"
     t.datetime "updated_at", null: false
     t.index ["chat_id"], name: "index_messages_on_chat_id"
   end
@@ -189,7 +193,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_27_093420) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-  
+
+  add_foreign_key "chats", "meals"
   add_foreign_key "messages", "chats"
   add_foreign_key "solid_queue_blocked_executions", "solid_queue_jobs", column: "job_id", on_delete: :cascade
   add_foreign_key "solid_queue_claimed_executions", "solid_queue_jobs", column: "job_id", on_delete: :cascade
