cb8a352b60f04ddfda687053385379ece542d6b1
Daniel dos Santos <ddsantos@gmx.de>
2026-05-27 11:55:28 +0200

    install Devise, create user devise, add views. implement flashes and add to applications body

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  # Protect every route by default
+  before_action :authenticate_user!
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
