4c6955e5980524c372ca996a651b78cc5352d1b4
Carl Furthmann <carl.furthi@gmail.com>
2026-05-27 12:31:42 +0200

    Update db/migrate/20260527093109_create_messages.rb
    Co-authored-by: adrianoterrazas2019-web <adrianoterrazas2019@gmail.com>

--- db/migrate/20260527093109_create_messages.rb
+++ db/migrate/20260527093109_create_messages.rb
@@ -2,6 +2,7 @@ class CreateMessages < ActiveRecord::Migration[8.1]
   def change
     create_table :messages do |t|
       t.text :content
+      t.string :role
       t.references :chat, null: false, foreign_key: true
 
       t.timestamps
