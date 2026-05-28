ba086e5c37e5becbc204850fe63d295041e6c024
Daniel dos Santos <ddsantos@gmx.de>
2026-05-28 11:00:56 +0200

    rename chat_controller to chats_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get "/chat", to: "chat#index"
   devise_for :users
   root "pages#home"
-  resources :meals, only: [ :index, :show, :destroy ]
+  resources :meals, only: [ :index, :show, :new, :create, :destroy ]
+  resources :chats, only: [ :show ] do
+    resources :messages, only: [ :create ]
+  end
 end
