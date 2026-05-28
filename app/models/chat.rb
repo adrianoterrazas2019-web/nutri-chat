100d42dd9c29e47cb84b4506328d95b62f5ebeea
Daniel dos Santos <ddsantos@gmx.de>
2026-05-28 16:20:20 +0200

    fix chat-user relationships

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,5 +1,4 @@
 class Chat < ApplicationRecord
   has_many :messages, dependent: :destroy
   belongs_to :meal
-  has_many :messages, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,4 @@
 class User < ApplicationRecord
-  has_many :chats, dependent: :destroy
-  has_many :meals, through: :chats, dependent: :destroy
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
