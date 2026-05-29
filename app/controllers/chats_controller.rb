43ad96dd706ea461363ecc7b4f4f59d06e8fdd5c
Daniel dos Santos <ddsantos@gmx.de>
2026-05-29 10:54:54 +0200

    add new chat to meal show page

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -4,4 +4,18 @@ class ChatsController < ApplicationController
     @meal = @chat.meal
     @message = Message.new
   end
+
+  def create
+    @meal = Meal.find(params[:meal_id])
+
+    @chat = Chat.new(title: Chat::DEFAULT_TITLE)
+    @chat.meal = @meal
+
+    if @chat.save
+      redirect_to chat_path(@chat)
+    else
+      @chats = @meal.chats.where(user: current_user)
+      render meal_path(@meal) # "meals/:id"
+    end
+  end
 end
